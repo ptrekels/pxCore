414e5cf91ff75b4a5144b0bede968b218c8bc227
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2018-02-16 16:24:13 +0100

    time related functions - implementation cleanup
     - introduces new common implementation of time related functions
       for all platforms supporting c++11,
     - try to synchronize legacy (non c++11) time implementations,
     - fixes legacy (non c++11) implementation of pxSleepMS(),
     - introduces new: void pxSleepUS(uint64_t usToSleep); function,
    
     - fixes pxTimerNativeTest failure:
        /Users/travis/build/pxscene/pxCore/tests/pxScene2d/test_pxTimerNative.cpp:37: Failure
        Value of: (int)(endTime - startTime) == TEST_TIME
          Actual: false
        Expected: true
        [  FAILED  ] pxTimerNativeTest.pxTimerNativeTestS (2000 ms),
    
     - start using pxSleep*() in wayland_egl/pxWindowNative.cpp backend.

--- src/pxTimer.h
+++ src/pxTimer.h
@@ -1,6 +1,6 @@
 /*
 
- pxCore Copyright 2005-2017 John Robinson
+   pxCore Copyright 2005-2018 John Robinson
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -23,10 +23,47 @@
 
 #include <inttypes.h>
 
-double pxSeconds();
-double pxMilliseconds();
-double pxMicroseconds();
+#if __cplusplus >= 201103L // {
+    // Common implementation for all modern platforms supporting >= c++11
 
-void pxSleepMS(uint32_t msToSleep);
+#   include <chrono>
+#   include <ratio>
+#   include <thread>
 
-#endif
+    inline double pxMicroseconds()
+    {
+        auto t_now = std::chrono::high_resolution_clock::now();
+        auto t_us = std::chrono::time_point_cast<std::chrono::microseconds>(t_now).time_since_epoch();
+        return t_us.count();
+    }
+
+    inline double pxMilliseconds()
+    {
+        return pxMicroseconds() / 1000.0;
+    }
+
+    inline double pxSeconds()
+    {
+        return pxMilliseconds() / 1000.0;
+    }
+
+    inline void pxSleepUS(const uint64_t usToSleep)
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(usToSleep));
+    }
+
+    inline void pxSleepMS(const uint32_t msToSleep)
+    {
+        pxSleepUS(msToSleep * 1000UL);
+    }
+#else // }{
+    // Legacy, platform specific, implementation for old platforms not supporting c++11
+    double pxMicroseconds();
+    double pxMilliseconds();
+    double pxSeconds();
+
+    void pxSleepMS(uint32_t msToSleep);
+    void pxSleepUS(uint64_t usToSleep);
+#endif // } __cplusplus >= 201103L
+
+#endif // PX_TIMER_H
